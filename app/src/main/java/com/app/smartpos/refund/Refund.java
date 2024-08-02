34ad0ff200fb0aba32c98fd9a3a36edd4725bf7a
karimsaad687 <karimsaad687@gmail.com>
2024-08-02 14:15:31 +0300

    first refund screen ui

---
+++ app/src/main/java/com/app/smartpos/refund/Refund.java
@@ -0,0 +1,16 @@
+package com.app.smartpos.refund;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.smartpos.R;
+
+public class Refund extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_refund);
+    }
+}
