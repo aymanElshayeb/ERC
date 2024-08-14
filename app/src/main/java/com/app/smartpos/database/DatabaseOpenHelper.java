4f8dc1a4d2c944c92ae995c70f31a3d96417fa4b
mostafa abbas <mmabbas@ejada.com>
2024-08-14 11:06:48 +0300

    update records

--- app/src/main/java/com/app/smartpos/database/DatabaseOpenHelper.java
+++ app/src/main/java/com/app/smartpos/database/DatabaseOpenHelper.java
@@ -19,7 +19,7 @@ import es.dmoral.toasty.Toasty;
 
 public class DatabaseOpenHelper extends SQLiteAssetHelper {
     public static final String DATABASE_NAME = "smart_pos.db";
-    private static final int DATABASE_VERSION = 38;
+    private static final int DATABASE_VERSION = 39;
     private Context mContext;
 
     public DatabaseOpenHelper(Context context) {
