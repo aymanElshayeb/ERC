a1514fab21d8c63b80a5b19bca0c8d662a2858eb
karimsaad687 <karimsaad687@gmail.com>
2024-09-22 14:51:34 +0300

    try another solution to disable certificate check

--- app/src/main/java/com/app/smartpos/SplashActivity.java
+++ app/src/main/java/com/app/smartpos/SplashActivity.java
@@ -18,9 +18,13 @@ import com.app.smartpos.utils.Hasher;
 
 import org.apache.http.auth.AUTH;
 
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSession;
 import javax.net.ssl.SSLSocketFactory;
@@ -50,7 +54,7 @@ public class SplashActivity extends AppCompatActivity {
         String endDateString = databaseAccess.getLastShift("end_date_time");
         Log.i("end_date", endDateString);
 
-
+        disableSSLCertificateChecking();
         AndroidNetworking.initialize(this,getUnsafeOkHttpClient());
 
         new Handler().postDelayed(new Runnable() {
@@ -105,6 +109,46 @@ public class SplashActivity extends AppCompatActivity {
             return okHttpClient;
         } catch (Exception e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Disables the SSL certificate checking for new instances of {@link HttpsURLConnection} This has been created to
+     * aid testing on a local box, not for use on production.
+     */
+    public static void disableSSLCertificateChecking() {
+        TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+            public X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+
+            @Override
+            public void checkClientTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+                // Not implemented
+            }
+
+            @Override
+            public void checkServerTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+                // Not implemented
+            }
+        }};
+
+        try {
+            SSLContext sc = SSLContext.getInstance("TLS");
+
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            });
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
         }
     }
 }
